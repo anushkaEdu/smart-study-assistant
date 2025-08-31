8e00a15c411ea61a13a5a2361dc116c10493d441
Anushka Kanaujia <anushka.23FE10CAI00410@muj.manipal.edu>
2025-08-31 21:45:34 +0530

    Fix NLTK punkt download for Streamlit deployment

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from transformers import pipeline
 import nltk
 import os
 
-# Ensure NLTK punkt tokenizer is downloaded in a local folder
-nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
-os.makedirs(nltk_data_dir, exist_ok=True)
+# Make sure nltk downloads punkt to a temporary folder (works on Streamlit Cloud)
+nltk_data_dir = "/tmp/nltk_data"
 nltk.data.path.append(nltk_data_dir)
 
 try:
     nltk.data.find("tokenizers/punkt")
 except LookupError:
     nltk.download("punkt", download_dir=nltk_data_dir)
+
 
 # Load summarization model
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
